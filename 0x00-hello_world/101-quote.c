b8629ec92e49d3bbf1e76df0fd7cc20ef2fb43c8
Daniel Solomon <danielsolomon696@gmail.com>
2023-03-16 15:03:57 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A c program that prints a line to the standard error
  * Return 1 (Success)
@@ -7,5 +8,5 @@
 int main(void)
 { 
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	return (1);
 }
