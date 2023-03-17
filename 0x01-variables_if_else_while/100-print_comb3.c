ca7feee1a89eb06fbce18848b81e9329ee2fadde
Daniel Solomon <danielsolomon696@gmail.com>
2023-03-17 10:09:09 -0700

    Variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combination of two digits
+ * main - prints all possible different combinations of two digits
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -24,6 +24,6 @@ int main(void)
 			}
 		}
 	}
-	putcchar('\n');
+	putchar('\n');
 	return (0);
 }			
