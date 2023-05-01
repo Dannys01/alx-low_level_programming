913b6be3968e499bbafa482ebea3c54f47da7bf0
Dannys01 <danielsolomon696@gmail.com>
2023-05-01 04:26:33 -0700

    0x013

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#indef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdio.h>
@@ -7,7 +7,7 @@
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: ppints to the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for alx project
@@ -29,7 +29,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-kistint_t *reverse_listint(listint_t **head);
+listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
